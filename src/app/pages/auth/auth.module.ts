f1cda871aa40c6c705c55d93d26e649fa08210ef
Paolo Tesser <p.tesser921@gmail.com>
2019-04-18 10:43:22 +0200

    fix(paths): add @ on internal paths to the components

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthComponent } from './auth.component';
+import { AuthComponent } from '@pages/auth/auth.component';
 
 const ROUTES: Routes = [
   {
--- src/app/pages/noauth/noauth.module.ts
+++ src/app/pages/noauth/noauth.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { NoauthComponent } from './noauth.component';
+import { HomeComponent } from '@pages/noauth/home/home.component';
+import { NoauthComponent } from '@pages/noauth/noauth.component';
 
 const ROUTES: Routes = [
   {
