60fca7718180c02385142c5508c7d5818c36f2e1
Luca Sanelli <lsanelli@dyn200.lnf.infn.it>
2019-04-18 09:54:54 +0200

    setup progetto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import {PagesModule} from "./pages/pages.module";
+import {SharedModule} from "./shared/shared.module";
+import {UiModule} from "./ui/ui.module";
+
+const MODULES = [CoreModule ,PagesModule,SharedModule,UiModule];
 
 @NgModule({
   declarations: [
@@ -11,6 +17,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ...MODULES
   ],
   providers: [],
   bootstrap: [AppComponent],
